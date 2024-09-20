c727dadc80e8d5a2282d70470849d550a3a986d3
React.js <aditya@192.168.1.5>
2024-09-20 14:11:59 +0530

    adding changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Weather from "./components/Weather";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <>
       <Weather />
+      <Analytics />
     </>
   );
 }
